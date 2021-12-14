c1b9488f8c34f89ae26b77b4faddf674e83d4830
smart0815 <sandy815.dev@outlook.com>
2021-12-14 23:01:01 +0800

    Update: wallet collector file

--- walletCollector.js
+++ walletCollector.js
@@ -36,9 +36,9 @@ async function walletCollector(finalOutput, key) {
 	// return finalOutput.filter((entry) => entry.balance != undefined).reverse();
 	try {
 		const characterPromises = array.map((character, i) =>
-			// 	// console.log(character)
+				console.log(character)
 			// addOrUpdateWalletInfo(array)
-			addOrUpdateWalletInfo({ ...character, ID: i + '' })
+			// addOrUpdateWalletInfo({ ...character, ID: i + '' })
 		);
 		console.log('nnnnnnnnnnnn');
 		await Promise.all(characterPromises);
