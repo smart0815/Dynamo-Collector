97159c1651ca6ce7fb0cf866d4a1cf62560245aa
smart0815 <sandy815.dev@outlook.com>
2021-12-17 01:31:09 +0800

    Update: app file

--- app.js
+++ app.js
@@ -13,15 +13,17 @@ app.use(cors());
 
 // parse requests of content-type - application/json
 app.use(express.json());  /* bodyParser.json() is deprecated */
-app.use(header_middleware);
+const bodyParser = require('body-parser'); 
+app.use(bodyParser.json()); 
 
+app.use(header_middleware);
 //The default request size is 100kb in body-parser
-var bodyParser = require('body-parser');
-// app.use(bodyParser.json({limit: '50mb'}));
-// app.use(express.json({limit:'50mb'}));
-// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+// var bodyParser = require('body-parser');
+// // app.use(bodyParser.json({limit: '50mb'}));
+// // app.use(express.json({limit:'50mb'}));
+// // app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+// app.use(bodyParser.json({ limit: "50mb" }));
+// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 
 // parse requests of content-type - application/x-www-form-urlencoded
 // app.use(express.urlencoded({ extended: true }));
