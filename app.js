7c561d961b9839f0311efd5d557d5e907a540183
smart0815 <sandy815.dev@outlook.com>
2021-12-17 00:40:12 +0800

    Update: limit capacity

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.use(express.json());  /* bodyParser.json() is deprecated */
 app.use(header_middleware);
 
 //The default request size is 100kb in body-parser
-app.use(express.json({limit:'50mb'}));
+var bodyParser = require('body-parser');
+// app.use(bodyParser.json({limit: '50mb'}));
+// app.use(express.json({limit:'50mb'}));
+// app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
