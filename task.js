02ae17bedc33bfca454bad2cb106727bd10b0902
smart0815 <sandy815.dev@outlook.com>
2021-12-18 04:21:32 +0800

    Update: task component

--- task.js
+++ task.js
@@ -11,9 +11,55 @@ AWS.config.update({
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const TASK_TABLE = 'task_status';
 const FLAG_TABLE = 'Flag'
+const {
+	getTransaction,
+} = require('./wallet');
+
+const getFlagStatus = async () => {
+	const params = {
+		TableName: FLAG_TABLE
+	};
+	return await dynamoClient.scan(params).promise();
+};
+
+const updateFlagStatus = async (character) => {
+	const params = {
+		TableName: FLAG_TABLE,
+		Item: character,
+	};
+	return await dynamoClient.put(params).promise();
+};
 
 const getTaskInfo = async (address) => {
-	console.log("here");
+	const status = await getFlagStatus();
+	console.log(status);
+
+	var params = {
+		TableName: TASK_TABLE,
+		KeyConditionExpression: "#cat = :findValue",
+		FilterExpression: '#cat = :findValue',
+		ExpressionAttributeNames: {
+			'#cat': 'status',
+		},
+		ExpressionAttributeValues: {
+			':findValue': address,
+		},
+	};
+
+	var taskInfo = await dynamoClient.scan(params).promise();
+	console.log(taskInfo);
+	if (taskInfo && status.Items[0]["Flag"]) {
+		if (taskInfo.Items[0].character == 'wallet') {
+			var arr = [];
+			arr.ID = 1;
+			arr.Flag = true;
+			updateFlagStatus(arr);
+			getTransaction(taskInfo.Items[0].param);
+			console.log("really?");
+		}
+	}
 };
 
 getTaskInfo(false);
+// getFlagStatus(1);
+// addOrUpdateCharacter(hp);
