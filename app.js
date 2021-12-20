4d246a072dcc4dce7c3c52da56eb1cb5f88eda36
smart0815 <sandy815.dev@outlook.com>
2021-12-20 11:39:13 +0800

    Update: dependency

--- app.js
+++ app.js
@@ -71,14 +71,16 @@ app.get('/walletInfo/:id', async (req, res) => {
 		const character = await getWalletInfo(id);
 		const taskInfo = await getTaskInfo(id);
 		console.log(character, taskInfo.Items);
-		if (!character.length && !taskInfo.Items) {
-			console.log('llllllllllllllllllllllllllllllllllllllllllllllllllllllll');
-			const array = [];
-			array.ID = new Date().getTime();
-			array.status = false;
-			array.character = "wallet";
-			array.param = id;
-			addUpdateTask(array);
+		if (!character.length) {
+			if(!taskInfo.Items) {
+				console.log('llllllllllllllllllllllllllllllllllllllllllllllllllllllll');
+				const array = [];
+				array.ID = new Date().getTime();
+				array.status = false;
+				array.character = "wallet";
+				array.param = id;
+				addUpdateTask(array);
+			}
 		}
 		res.send(character);
 	} catch (err) {
