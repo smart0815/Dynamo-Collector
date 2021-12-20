7eb3ecd82cf5046438b6ae711337811da9c97248
smart0815 <sandy815.dev@outlook.com>
2021-12-20 16:28:57 +0800

    Update: response body file

--- transaction.js
+++ transaction.js
@@ -51,7 +51,7 @@ async function transactionInfo(key) {
 		}
 		
 		console.log('nmm,nmnnmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
-		console.log(await response.send());
+		console.log(await response.json());
 		console.log('nmm,nmnnmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm')
 		let json = await response.json();
 		let result = json.data;
