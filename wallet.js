11c5449f3ddb6f3631395abfa7ffc6eba7671b97
smart0815 <sandy815.dev@outlook.com>
2021-12-22 03:57:48 +0800

    Update: collector server

--- wallet.js
+++ wallet.js
@@ -4,6 +4,7 @@ import { addOrUpdateWalletInfo, updateTaskInfo } from './dynamo1.js';
 let milliseconds = 11000;
 const MAINNET_URL_API = "https://solana--mainnet.datahub.figment.io/apikey/ef802cd19ef5d8638c6a6cbbcd1d3144/";
 const SERVER_URL_API = "http://ec2-18-191-149-176.us-east-2.compute.amazonaws.com:8080/walletCollector/";
+const SERVER1_URL_API = "http://ec2-3-144-143-163.us-east-2.compute.amazonaws.com:8080/walletCollector/";
 
 async function getResults(before, key) {
 	const response = await fetch(`${MAINNET_URL_API}`, {
@@ -48,7 +49,7 @@ export async function getWalletInfo(key) {
 	}
 	// console.log(finalOutput);
 	let count = finalOutput.length % 2 == 0 ? finalOutput.length / 2 : finalOutput.length / 2 + 0.5;
-
+	let count  = parseInt(finalOutput.length/3);
 	fetch(`${SERVER_URL_API}`, {
 		method: 'POST',
 		headers: {
@@ -60,7 +61,18 @@ export async function getWalletInfo(key) {
 		})
 	}).catch(err => console.error(err, ""));
 
-	const firstOut = finalOutput.slice(count, finalOutput.length);
+	fetch(`${SERVER1_URL_API}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			"address": key,
+			"params": finalOutput.slice(count, count*2)
+		})
+	}).catch(err => console.error(err, ""));
+
+	const firstOut = finalOutput.slice(count*2, finalOutput.length);
 	let signatureBalance;
 	let balance;
 	var number;
