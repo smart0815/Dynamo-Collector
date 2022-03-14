863cfe2e50516c9ddfec15788da9ce37b8653e19
smart0815 <sandy815.dev@outlook.com>
2022-03-14 20:37:18 +0800

    Added ethereum purchaser collector func

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(header_middleware);
 
 import { walletCollector } from './walletCollector.js';
 import { purchaserCollector } from './purchaserCollector.js';
+import { getEthereumPurchaserCollector } from './ethereumPurchaserCollector.js';
 // const { getTransaction } = require('./wallet');
 // app.use(cors());
 
@@ -45,6 +46,19 @@ app.post('/purchaserCollector', async (req, res) => {
 
 	try {
 		const character = await purchaserCollector(req.body);
+		res.json(character);
+	} catch (err) {
+		console.error(err);
+	}
+});
+
+app.post('/ethereumPurchaserCollector', async (req, res) => {
+	app.use(bodyParser.json());
+	app.use(bodyParser.json({ limit: '50mb' }));
+	app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+
+	try {
+		const character = await getEthereumPurchaserCollector(req.body);
 		res.json(character);
 	} catch (err) {
 		console.error(err);
