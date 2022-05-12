b0ddb4b29510a0bc1fbab0ca989e1f698179a15e
smart0815 <jack11112011@outlook.com>
2022-05-13 04:13:04 +0800

    Update mainnet url

--- walletCollector.js
+++ walletCollector.js
@@ -5,7 +5,7 @@ import { decodeMetadata, getMetadataAccount } from "./Metadata.service.js";
 import { clusterApiUrl, Connection, PublicKey } from "@solana/web3.js";
 let connection = new Connection(clusterApiUrl('mainnet-beta'), 'confirmed');
 let milliseconds = 11000;
-const MAINNET_URL_API = "https://api.mainnet-beta.solana.com";
+const MAINNET_URL_API = "https://solana--mainnet.datahub.figment.io/apikey/ef802cd19ef5d8638c6a6cbbcd1d3144/";
 
 const AWS_SERVER_TABLE = 'server_status';
 const WALLET_TABLE = 'Wallet_history';
@@ -48,38 +48,40 @@ export async function walletCollector(walletParams) {
 
 	var finalOutput = walletParams.params;
 	for (const iterator of finalOutput) {
-		if (!iterator.err) {
-			for (let i = 0; i < 20; i++) {
-				try {
-					signatureBalance = await fetch(`${MAINNET_URL_API}`, {
-						method: 'POST',
-						headers: {
-							'Content-Type': 'application/json',
-						},
-						body: JSON.stringify({
-							"jsonrpc": "2.0",
-							"id": 2,
-							"method": "getTransaction",
-							"params": [
-								iterator.signature,
-								"json"
-							]
-						})
-					});
-					balance = await signatureBalance.json();
-					if (balance.result === null) {
-						await delay(milliseconds); // Before re-trying the next loop cycle, let's wait 5 seconds (5000ms)
-						continue;
-					} else {
-						break;
-					}
-				} catch {
-					await delay(milliseconds);
+		for (let i = 0; i < 20; i++) {
+			try {
+				signatureBalance = await fetch(`${MAINNET_URL_API}`, {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						"jsonrpc": "2.0",
+						"id": 2,
+						"method": "getTransaction",
+						"params": [
+							iterator.signature,
+							"json"
+						]
+					})
+				});
+				balance = await signatureBalance.json();
+				if (balance.result === null) {
+					await delay(milliseconds); // Before re-trying the next loop cycle, let's wait 5 seconds (5000ms)
 					continue;
+				} else {
+					break;
 				}
+			} catch {
+				await delay(milliseconds);
+				continue;
 			}
-			let index;
-			if (balance) {
+		}
+		let index;
+		if (balance) {
+			if (iterator.err) {
+				iterator.balance = balance["result"]?.meta["postBalances"][index] - balance["result"]?.meta["preBalances"][index];
+			} else {
 				if (balance["result"]?.meta["postTokenBalances"]?.length) {
 					const items = [];
 					console.log(balance["result"].meta["postTokenBalances"][0].mint);
@@ -93,10 +95,10 @@ export async function walletCollector(walletParams) {
 						if (elem?.data.uri) {
 							if (!elem.data.uri.includes("details.txt")) {
 								try {
-									let nftMetadtacontent = await fetch(elem.data.uri);	
+									let nftMetadtacontent = await fetch(elem.data.uri);
 									iterator.nftMetaData = await nftMetadtacontent.text();
 								} catch (error) {
-								} 
+								}
 							}
 						}
 						else {
